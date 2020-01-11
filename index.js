f4c18ab1909236d3265d748a745c4aa576a93a0a
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-12 00:29:42 +0100

    removed react navigation wrapper

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import NavigationWrapper from './app/navigation/navigation-wrapper';
+import App from './App';
 
-AppRegistry.registerComponent(appName, () => NavigationWrapper);
+AppRegistry.registerComponent(appName, () => App);
